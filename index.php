f14a654b2a84a193d5f2c342cd8e874f97e37e5d
Gabriel447 <Gabriel_447@live.com>
2022-10-19 16:05:35 -0300

    comments

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
+//você precisa envolvar o pdo com try/cath para não vazar a senha do banco
+
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=devweb', 'root', '');
-    //modo de desenvolvimento
+    //modo de desenvolvimento (inserir linha abaixo para debug)
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //modo de produção não precisa
 } catch (Exception $e) {
     //echo $e->getMessage();
     echo 'Não foi possível conectar! Tente novamente mais tarde!';
